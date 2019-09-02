59f1fdcfd266d286d64067699177020c45a1c3a8
Cxx0822 <1556464090@qq.com>
2019-09-02 16:28:09 +0800

    Predict Model

--- predict.py
+++ predict.py
@@ -19,21 +19,23 @@ resize_height = my_data['resize_height']
 logs_dir = my_data['logs_dir']
 
 my_labels = my_data['my_labels']
+val_tfrecord = my_data['val_tfrecord']
 
 
-def my_predict():
-    CAPACITY = 200
-    record_file = 'dataSet/record/val{}.tfrecords'.format(resize_height)
+def my_predict_queue():
+    record_file = val_tfrecord
 
     sess = tf.Session()
-
+    # 加载数据集
     tf_image, tf_label = read_records(record_file, IMG_SIZE, IMG_SIZE, type='normalization')
-    image_train_batch, label_train_batch = get_batch_images(tf_image, tf_label, batch_size=BATCH_SIZE,
-                                                            labels_nums=N_CLASSES, one_hot=False, shuffle=False)
+    image_val_batch, label_val_batch = get_batch_images(tf_image, tf_label, batch_size=BATCH_SIZE,
+                                                        labels_nums=N_CLASSES, one_hot=False, shuffle=True)
 
-    train_logits = inference_op(image_train_batch, 0.5, N_CLASSES)
-    train_logits = tf.nn.softmax(train_logits)  
+    # 预测模型
+    val_logits = inference_op(image_val_batch, N_CLASSES)
+    val_logits = tf.nn.softmax(val_logits)  
 
+    # 加载模型
     saver = tf.train.Saver()
     print('\nLoading checkpoints...')
     ckpt = tf.train.get_checkpoint_state(logs_dir)
@@ -43,17 +45,22 @@ def my_predict():
         print('Successful loading, global_step = %s\n' % global_step)
     else:
         print('No checkpoints')
+
+    # 启动queue
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     try:    
+        # 预测结果
         for step in range(MAX_STEP):
             if coord.should_stop():
                 break
 
-            image, prediction = sess.run([image_train_batch, train_logits])  
+            # 启动图片、预测
+            image, prediction = sess.run([image_val_batch, val_logits])  
             max_index = np.argmax(prediction)     
 
+            # 判断类别
             if max_index == 0:
                 label = '%.2f%% ' % (prediction[0][0] * 100) + 'is a ' + str(my_labels[0]) + '.'
             elif max_index == 1:
@@ -65,6 +72,7 @@ def my_predict():
             elif max_index == 4:
                 label = '%.2f%% ' % (prediction[0][4] * 100) + 'is a ' + str(my_labels[4]) + '.'
 
+            # 显示图片
             plt.imshow(image[0])
             plt.title(label)
             plt.show()
@@ -78,5 +86,64 @@ def my_predict():
     sess.close()
 
 
+def my_predict_tfData():
+    filename = tf.compat.v1.placeholder(tf.string, shape=[None])
+
+    with tf.compat.v1.Session() as sess:
+        # 加载数据集
+        val_dataset = tf.data.TFRecordDataset(filename)
+
+        val_dataset = val_dataset.map(parse_records).shuffle(10000).batch(1).repeat(MAX_STEP)
+
+        iterator = val_dataset.make_initializable_iterator()   # 生成迭代器
+        val_images, val_labels = iterator.get_next()
+
+        val_filename = []
+        val_filename.append(val_tfrecord)   # 将tfrecords文件添加到文件名列表中（run里面需要列表格式，直接使用字符串会报错）
+
+        # 预测模型
+        val_logits = inference_op(val_images, N_CLASSES)
+        val_logits = tf.nn.softmax(val_logits)  
+
+        # 启动线程
+        sess.run(tf.compat.v1.global_variables_initializer())
+        sess.run(iterator.initializer, feed_dict={filename: val_filename})
+
+        # 加载模型
+        saver = tf.compat.v1.train.Saver()
+        print('\nLoading checkpoints...')
+        ckpt = tf.train.get_checkpoint_state(logs_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            print('Successful loading, global_step = %s\n' % global_step)
+        else:
+            print('No checkpoints')
+
+        # 预测结果
+        for step in range(MAX_STEP):  
+            # 启动图片、预测
+            image, prediction = sess.run([val_images, val_logits]) 
+            max_index = np.argmax(prediction)     
+
+            # 判断类别
+            if max_index == 0:
+                label = '%.2f%% ' % (prediction[0][0] * 100) + 'is a ' + str(my_labels[0]) + '.'
+            elif max_index == 1:
+                label = '%.2f%% ' % (prediction[0][1] * 100) + 'is a ' + str(my_labels[1]) + '.'
+            elif max_index == 2:
+                label = '%.2f%% ' % (prediction[0][2] * 100) + 'is a ' + str(my_labels[2]) + '.'
+            elif max_index == 3:
+                label = '%.2f%% ' % (prediction[0][3] * 100) + 'is a ' + str(my_labels[3]) + '.'
+            elif max_index == 4:
+                label = '%.2f%% ' % (prediction[0][4] * 100) + 'is a ' + str(my_labels[4]) + '.'
+
+            # 显示图片
+            plt.imshow(image[0])
+            plt.title(label)
+            plt.show()
+
+
 if __name__ == '__main__':
-    my_predict()
+    my_predict_queue()
+    # my_predict_tfData()
